21da55585dc484ae04ac96e8217d553610bae043
micky387 <mickaelsaibi@free.fr>
2025-07-21 22:10:19 -0400

    sm8250: Firmware_loader: Import the Asus fw loader changes
    Change-Id: Ie30ec2e3653e0a3dfccb92c9b3a112c7514cba4d
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -308,6 +308,16 @@ fw_get_filesystem_firmware(struct device *device, struct fw_priv *fw_priv)
 	enum kernel_read_file_id id = READING_FIRMWARE;
 	size_t msize = INT_MAX;
 
+#ifdef ZS670KS
+	char fw_name[5];
+	bool is_adsp_readed = false;
+	if(!strncmp(fw_priv->fw_name, "tfa98xx.cnt", 11))
+	{
+		dev_err(device, "%s() skip for fw_name (%s)\n", __func__, fw_priv->fw_name);
+		return -2;
+	}
+#endif  //ZS670KS
+
 	/* Already populated data member means we're loading into a buffer */
 	if (fw_priv->data) {
 		id = READING_FIRMWARE_PREALLOC_BUFFER;
@@ -329,6 +339,42 @@ fw_get_filesystem_firmware(struct device *device, struct fw_priv *fw_priv)
 			rc = -ENAMETOOLONG;
 			break;
 		}
+
+#ifdef ZS670KS
+		/* ASUS BSP : For Change ADSP FW loading path to vendor/firmware */
+		snprintf(fw_name, 5, "%s", fw_priv->fw_name);
+		if (!strcmp(fw_name, "adsp")  &&  is_adsp_readed == false ) {
+			is_adsp_readed = true;
+			snprintf(path, PATH_MAX, "%s/%s", "/system/vendor/firmware/q6_ZS670KS_sm8250_image", fw_priv->fw_name);
+			dev_err(device, "[ADSP] Try to load firmware : %s \n", path);
+		}
+
+		/* BSP_WIFI +++ */
+		if (!strncmp(fw_priv->fw_name, "qca6390/bdwlan", 14)) {
+				snprintf(path, PATH_MAX, "%s", "/vendor/firmware/bdwlan.elf");
+				dev_err(device, "[wlan] Try to load firmware : %s \n", path);
+		}
+		if (!strncmp(fw_priv->fw_name, "qca6390/amss20", 14)) {
+				snprintf(path, PATH_MAX, "%s", "/vendor/firmware/amss20.bin");
+				dev_err(device, "[wlan] Try to load firmware : %s \n", path);
+		}
+		if (!strncmp(fw_priv->fw_name, "qca6390/regdb", 13)) {
+				snprintf(path, PATH_MAX, "%s", "/vendor/firmware/regdb.bin");
+				dev_err(device, "[wlan] Try to load firmware : %s \n", path);
+		}
+		if (!strncmp(fw_priv->fw_name, "qca6390/m3.bin", 14)) {
+				snprintf(path, PATH_MAX, "%s", "/vendor/firmware/m3.bin");
+				dev_err(device, "[wlan] Try to load firmware : %s \n", path);
+		}
+		/* BSP_WIFI ---*/
+
+		/* ASUS BSP : For Change Sensor Core FW loading path */
+		if (!strncmp(fw_priv->fw_name, "slpi", 4)) {
+			snprintf(path, PATH_MAX, "%s/%s", "/vendor/firmware", fw_priv->fw_name);
+			dev_err(device, "[SLPI] Try to load firmware : %s \n", path);
+		}
+		/* ASUS BSP ---*/
+#endif  //ZS670KS
 
 		fw_priv->size = 0;
 		rc = kernel_read_file_from_path(path, &fw_priv->data, &size,
