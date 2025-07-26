ed192709bf7511d5695e9bcb8a09d7aca5c62d1a
micky387 <mickaelsaibi@free.fr>
2025-07-26 17:22:50 -0400

    sm8250: Import the Asus's mmc sdhci sdcard power-off sequence
    Change-Id: I2eccb72959be98a4b5fe39ea13f8da274f3024e6
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2901,8 +2901,21 @@ static int sdhci_msm_setup_vreg(struct sdhci_msm_pltfm_data *pdata,
 		goto out;
 	}
 
+#ifdef ZS670KS
+	//ASUS_SZ_BSP  bevis: for zf7 sdcard power-off sequence +++
+	if(!strncmp(mmc_hostname(pdata->mmc),"mmc0",strlen("mmc0")) && !enable)
+	{
+		vreg_table[1] = curr_slot->vdd_data;
+		vreg_table[0] = curr_slot->vdd_io_data;
+	}else{
+		vreg_table[0] = curr_slot->vdd_data;
+		vreg_table[1] = curr_slot->vdd_io_data;
+	}
+	//ASUS_SZ_BSP  bevis: for zf7 sdcard power-off sequence ---
+#else
 	vreg_table[0] = curr_slot->vdd_data;
 	vreg_table[1] = curr_slot->vdd_io_data;
+#endif
 
 	for (i = 0; i < ARRAY_SIZE(vreg_table); i++) {
 		if (vreg_table[i]) {
